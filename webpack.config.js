63860e6a980f4ef5e531fc56f9559dcd9037a6b7
Matt Hamil <j.matthew.hamil@gmail.com>
2018-12-16 15:40:17 -0600

    chore: initial webpack setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,22 @@
 module.exports = {
-  entry: './src/index.tsx',
+  entry: "./src/index.ts",
   output: {
-    filename: 'bundle.js',
-    path: __dirname + '/dist'
+    filename: "bundle.js",
+    path: __dirname + "/dist"
   },
 
-  devtool: 'source-map',
+  devtool: "source-map",
 
   resolve: {
-    extensions: ['.ts', '.js', '.json']
+    extensions: [".ts", ".js", ".json"]
   },
 
   module: {
     rules: [
-      { test: /\.tsx?$/, loader: 'awesome-typescript-loader' },
+      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
 
       // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-      { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' }
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
     ]
   }
 };
