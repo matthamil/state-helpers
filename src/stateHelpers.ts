b50536af2a7e5f586bf3e2f6d9d5747750be1af3
Matt Hamil <j.matthew.hamil@gmail.com>
2018-12-16 16:59:15 -0600

    chore: refactor updateWhere and removeWhere

--- src/stateHelpers.ts
+++ src/stateHelpers.ts
@@ -3,18 +3,7 @@ type StateUpdater = (state: State) => State;
 type Comparator = (value: any) => boolean;
 type Updater = (value: any) => any;
 
-export function removeWhere(key: string, comparator: Comparator): StateUpdater {
-  function inverseComparator(value: any): boolean {
-    return !comparator(value);
-  }
-
-  return function removeAtKey(state: State): State {
-    return {
-      ...state,
-      [key]: state[key].filter(inverseComparator)
-    };
-  };
-}
+export { removeWhere } from "./removeWhere";
 
 export function updateWhere(
   key: string,
